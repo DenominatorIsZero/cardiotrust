6e156c51ace6fd98014ccab98dc7a23de9e29c3b
Erik Engelhardt <eren@tf.uni-kiel.de>
2023-06-07 15:50:26 +0200

    Implemented calculation of kalman gain

--- src/core/model/functional/kalman.rs
+++ src/core/model/functional/kalman.rs
@@ -1,4 +1,7 @@
+use approx::relative_eq;
 use ndarray::Array2;
+use ndarray_linalg::Inverse;
+use rand_distr::{Distribution, Normal};
 
 use crate::core::config::model::Model;
 
@@ -16,11 +19,53 @@ impl KalmanGain {
         }
     }
 
-    pub fn from_model_config(
-        _config: &Model,
-        _measurement_matrix: &MeasurementMatrix,
-    ) -> KalmanGain {
-        todo!()
+    pub fn from_model_config(config: &Model, measurement_matrix: &MeasurementMatrix) -> KalmanGain {
+        let mut process_covariance = Array2::<f32>::zeros((
+            measurement_matrix.values.shape()[1],
+            measurement_matrix.values.shape()[1],
+        ));
+        let mut measurement_covariance = Array2::<f32>::zeros((
+            measurement_matrix.values.shape()[0],
+            measurement_matrix.values.shape()[0],
+        ));
+
+        if relative_eq!(config.process_covariance_std, 0.0) {
+            process_covariance
+                .diag_mut()
+                .fill(config.process_covariance_mean);
+        } else {
+            let normal = Normal::<f32>::new(
+                config.process_covariance_mean,
+                config.process_covariance_std,
+            )
+            .unwrap();
+            process_covariance.diag_mut().iter_mut().for_each(|v| {
+                *v = normal.sample(&mut rand::thread_rng());
+            });
+        }
+
+        if relative_eq!(config.measurement_covariance_std, 0.0) {
+            measurement_covariance
+                .diag_mut()
+                .fill(config.measurement_covariance_mean);
+        } else {
+            let normal = Normal::<f32>::new(
+                config.measurement_covariance_mean,
+                config.measurement_covariance_std,
+            )
+            .unwrap();
+            measurement_covariance.diag_mut().iter_mut().for_each(|v| {
+                *v = normal.sample(&mut rand::thread_rng());
+            });
+        }
+
+        let h = &measurement_matrix.values;
+
+        let s = h.dot(&process_covariance).dot(&h.t()) + measurement_covariance;
+        let s_inv = s.inv().unwrap();
+        let k = process_covariance.dot(&h.t()).dot(&s_inv);
+
+        KalmanGain { values: k }
     }
 }
 
