1ad77ec2a4ab7ea910f487fc388597c3210ca665
Erik Engelhardt <eren@tf.uni-kiel.de>
2023-06-13 14:55:08 +0200

    Fixed bug with using izip!

--- src/core/model/functional/allpass.rs
+++ src/core/model/functional/allpass.rs
@@ -60,48 +60,52 @@ impl APParameters {
             }
             let (x_in, y_in, z_in) = input_voxel_index;
             let input_position_mm = &v_position_mm.slice(s![x_in, y_in, z_in, ..]);
-            for (x_offset, y_offset, z_offset) in izip!([-1, 0, 1], [-1, 0, 1], [-1, 0, 1]) {
-                if x_offset == 0 && y_offset == 0 && z_offset == 0 {
-                    continue;
-                }
-                let ouput_voxel_index = [
-                    x_in as i32 + x_offset,
-                    y_in as i32 + y_offset,
-                    z_in as i32 + z_offset,
-                ];
-                if !spatial_description.voxels.is_valid_index(ouput_voxel_index) {
-                    continue;
-                }
-                let [x_out, y_out, z_out] = [
-                    (x_in as i32 + x_offset) as usize,
-                    (y_in as i32 + y_offset) as usize,
-                    (z_in as i32 + z_offset) as usize,
-                ];
-                let output_position_mm = &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
+            for x_offset in -1..=1 {
+                for y_offset in -1..=1 {
+                    for z_offset in -1..=1 {
+                        if x_offset == 0 && y_offset == 0 && z_offset == 0 {
+                            continue;
+                        }
+                        let ouput_voxel_index = [
+                            x_in as i32 + x_offset,
+                            y_in as i32 + y_offset,
+                            z_in as i32 + z_offset,
+                        ];
+                        if !spatial_description.voxels.is_valid_index(ouput_voxel_index) {
+                            continue;
+                        }
+                        let [x_out, y_out, z_out] = [
+                            (x_in as i32 + x_offset) as usize,
+                            (y_in as i32 + y_offset) as usize,
+                            (z_in as i32 + z_offset) as usize,
+                        ];
+                        let output_position_mm = &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
 
-                let delay_s = calculate_delay_s(
-                    input_position_mm,
-                    output_position_mm,
-                    config.propagation_velocities_m_per_s.get(v_type).unwrap(),
-                );
-                let delay_samples = delay_s * sample_rate_hz;
+                        let delay_s = calculate_delay_s(
+                            input_position_mm,
+                            output_position_mm,
+                            config.propagation_velocities_m_per_s.get(v_type).unwrap(),
+                        );
+                        let delay_samples = delay_s * sample_rate_hz;
 
-                if delay_samples < 1.0 {
-                    return Err(format!(
-                        "Can not configure delays below 1 sample.\
+                        if delay_samples < 1.0 {
+                            return Err(format!(
+                                "Can not configure delays below 1 sample.\
                         Calculated delay: {:?}.\
                         For voxel type: {:?}",
-                        delay_samples, v_type
-                    )
-                    .into());
-                }
+                                delay_samples, v_type
+                            )
+                            .into());
+                        }
 
-                delays_samples.values[(
-                    v_numbers[input_voxel_index].unwrap() / 3,
-                    (1 + x_offset) as usize,
-                    (1 + y_offset) as usize,
-                    (1 + z_offset) as usize,
-                )] = delay_samples;
+                        delays_samples.values[(
+                            v_numbers[input_voxel_index].unwrap() / 3,
+                            (1 + x_offset) as usize,
+                            (1 + y_offset) as usize,
+                            (1 + z_offset) as usize,
+                        )] = delay_samples;
+                    }
+                }
             }
         }
 
@@ -141,93 +145,101 @@ impl APParameters {
                 .collect();
 
             for output_voxel_index in output_voxel_indices {
-                for (x_offset, y_offset, z_offset) in izip!([-1, 0, 1], [-1, 0, 1], [-1, 0, 1]) {
-                    // no self connection allowed
-                    if x_offset == 0 && y_offset == 0 && z_offset == 0 {
-                        continue;
-                    }
-                    let (x_out, y_out, z_out) = output_voxel_index;
-                    let input_voxel_index = [
-                        x_out as i32 - x_offset,
-                        y_out as i32 - y_offset,
-                        z_out as i32 - z_offset,
-                    ];
-                    // Skip if the input voxel doesn't exist
-                    if !spatial_description.voxels.is_valid_index(input_voxel_index) {
-                        continue;
-                    }
-                    let input_voxel_index = [
-                        (x_out as i32 - x_offset) as usize,
-                        (y_out as i32 - y_offset) as usize,
-                        (z_out as i32 - z_offset) as usize,
-                    ];
-                    // SKip if the input voxel is already connected
-                    if activation_time_s[input_voxel_index].is_some() {
-                        continue;
-                    }
-                    // Skip if connection is not allowed
-                    let output_voxel_type = &v_types[output_voxel_index];
-                    let input_voxel_type = &v_types[input_voxel_index];
-                    if !voxels::is_connection_allowed(output_voxel_type, input_voxel_type) {
-                        continue;
-                    }
-                    // Skip pathologies as anways if the propagation factor is zero
-                    if input_voxel_type == &VoxelType::Pathological
-                        && relative_eq!(config.current_factor_in_pathology, 0.0)
-                    {
-                        continue;
-                    }
+                for x_offset in -1..=1 {
+                    for y_offset in -1..=1 {
+                        for z_offset in -1..=1 {
+                            // no self connection allowed
+                            if x_offset == 0 && y_offset == 0 && z_offset == 0 {
+                                continue;
+                            }
+                            let (x_out, y_out, z_out) = output_voxel_index;
+                            let input_voxel_index = [
+                                x_out as i32 - x_offset,
+                                y_out as i32 - y_offset,
+                                z_out as i32 - z_offset,
+                            ];
+                            // Skip if the input voxel doesn't exist
+                            if !spatial_description.voxels.is_valid_index(input_voxel_index) {
+                                continue;
+                            }
+                            let input_voxel_index = [
+                                (x_out as i32 - x_offset) as usize,
+                                (y_out as i32 - y_offset) as usize,
+                                (z_out as i32 - z_offset) as usize,
+                            ];
+                            // SKip if the input voxel is already connected
+                            if activation_time_s[input_voxel_index].is_some() {
+                                continue;
+                            }
+                            // Skip if connection is not allowed
+                            let output_voxel_type = &v_types[output_voxel_index];
+                            let input_voxel_type = &v_types[input_voxel_index];
+                            if !voxels::is_connection_allowed(output_voxel_type, input_voxel_type) {
+                                continue;
+                            }
+                            // Skip pathologies as anways if the propagation factor is zero
+                            if input_voxel_type == &VoxelType::Pathological
+                                && relative_eq!(config.current_factor_in_pathology, 0.0)
+                            {
+                                continue;
+                            }
 
-                    // Now we finally found something that we want to connect.
-                    let input_state_number = v_numbers[input_voxel_index].unwrap();
+                            // Now we finally found something that we want to connect.
+                            let input_state_number = v_numbers[input_voxel_index].unwrap();
 
-                    let output_position_mm = &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
-                    let [x_in, y_in, z_in] = input_voxel_index;
-                    let input_position_mm = &v_position_mm.slice(s![x_in, y_in, z_in, ..]);
-                    let propagation_velocity_m_per_s = config
-                        .propagation_velocities_m_per_s
-                        .get(input_voxel_type)
-                        .unwrap();
+                            let output_position_mm =
+                                &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
+                            let [x_in, y_in, z_in] = input_voxel_index;
+                            let input_position_mm = &v_position_mm.slice(s![x_in, y_in, z_in, ..]);
+                            let propagation_velocity_m_per_s = config
+                                .propagation_velocities_m_per_s
+                                .get(input_voxel_type)
+                                .unwrap();
 
-                    let delay_s = calculate_delay_s(
-                        input_position_mm,
-                        output_position_mm,
-                        propagation_velocity_m_per_s,
-                    );
-                    let direction = calculate_direction(input_position_mm, output_position_mm);
+                            let delay_s = calculate_delay_s(
+                                input_position_mm,
+                                output_position_mm,
+                                propagation_velocity_m_per_s,
+                            );
+                            let direction =
+                                calculate_direction(input_position_mm, output_position_mm);
 
-                    // update activation time of input voxel, marking them as connected
-                    activation_time_s[input_voxel_index] =
-                        Some(activation_time_s[output_voxel_index].unwrap() + delay_s);
+                            // update activation time of input voxel, marking them as connected
+                            activation_time_s[input_voxel_index] =
+                                Some(activation_time_s[output_voxel_index].unwrap() + delay_s);
 
-                    current_directions
-                        .slice_mut(s![x_in, y_in, z_in, ..])
-                        .assign(&direction);
+                            current_directions
+                                .slice_mut(s![x_in, y_in, z_in, ..])
+                                .assign(&direction);
 
-                    let mut gain = calculate_gain(
-                        &direction,
-                        current_directions.slice(s![x_out, y_out, z_out, ..]),
-                    );
+                            let mut gain = calculate_gain(
+                                &direction,
+                                current_directions.slice(s![x_out, y_out, z_out, ..]),
+                            );
 
-                    if *input_voxel_type == VoxelType::Pathological
-                        && *output_voxel_type != VoxelType::Pathological
-                    {
-                        gain = gain * config.current_factor_in_pathology;
-                    }
-                    if *output_voxel_type == VoxelType::Pathological
-                        && *input_voxel_type != VoxelType::Pathological
-                    {
-                        gain = gain * (1.0 / config.current_factor_in_pathology);
-                    }
+                            if *input_voxel_type == VoxelType::Pathological
+                                && *output_voxel_type != VoxelType::Pathological
+                            {
+                                gain = gain * config.current_factor_in_pathology;
+                            }
+                            if *output_voxel_type == VoxelType::Pathological
+                                && *input_voxel_type != VoxelType::Pathological
+                            {
+                                gain = gain * (1.0 / config.current_factor_in_pathology);
+                            }
 
-                    for (input_dimension, output_dimension) in izip!(0..3, 0..3) {
-                        ap_params.gains.values[(
-                            input_state_number + input_dimension,
-                            (1 + x_offset) as usize,
-                            (1 + y_offset) as usize,
-                            (1 + z_offset) as usize,
-                            output_dimension,
-                        )] = gain[(input_dimension, output_dimension)];
+                            for input_dimension in 0..3 {
+                                for output_dimension in 0..3 {
+                                    ap_params.gains.values[(
+                                        input_state_number + input_dimension,
+                                        (1 + x_offset) as usize,
+                                        (1 + y_offset) as usize,
+                                        (1 + z_offset) as usize,
+                                        output_dimension,
+                                    )] = gain[(input_dimension, output_dimension)];
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -240,33 +252,37 @@ impl APParameters {
             .filter(|(_, v_type)| **v_type != VoxelType::None)
             .for_each(|(input_voxel_index, _)| {
                 let (x_in, y_in, z_in) = input_voxel_index;
-                for (x_offset, y_offset, z_offset) in izip!([-1, 0, 1], [-1, 0, 1], [-1, 0, 1]) {
-                    let ouput_voxel_index = [
-                        x_in as i32 + x_offset,
-                        y_in as i32 + y_offset,
-                        z_in as i32 + z_offset,
-                    ];
-                    if !spatial_description.voxels.is_valid_index(ouput_voxel_index) {
-                        continue;
-                    }
-                    let output_voxel_index = [
-                        (x_in as i32 + x_offset) as usize,
-                        (y_in as i32 + y_offset) as usize,
-                        (z_in as i32 + z_offset) as usize,
-                    ];
-                    for input_direction in 0..3 {
-                        let input_state_number =
-                            v_numbers[input_voxel_index].unwrap() + input_direction;
-                        for output_direction in 0..3 {
-                            let output_state_index =
-                                v_numbers[output_voxel_index].unwrap() + output_direction;
-                            ap_params.output_state_indices.values[(
-                                input_state_number,
-                                (1 + x_offset) as usize,
-                                (1 + y_offset) as usize,
-                                (1 + z_offset) as usize,
-                                output_direction,
-                            )] = Some(output_state_index);
+                for x_offset in -1..=1 {
+                    for y_offset in -1..=1 {
+                        for z_offset in -1..=1 {
+                            let ouput_voxel_index = [
+                                x_in as i32 + x_offset,
+                                y_in as i32 + y_offset,
+                                z_in as i32 + z_offset,
+                            ];
+                            if !spatial_description.voxels.is_valid_index(ouput_voxel_index) {
+                                continue;
+                            }
+                            let output_voxel_index = [
+                                (x_in as i32 + x_offset) as usize,
+                                (y_in as i32 + y_offset) as usize,
+                                (z_in as i32 + z_offset) as usize,
+                            ];
+                            for input_direction in 0..3 {
+                                let input_state_number =
+                                    v_numbers[input_voxel_index].unwrap() + input_direction;
+                                for output_direction in 0..3 {
+                                    let output_state_index =
+                                        v_numbers[output_voxel_index].unwrap() + output_direction;
+                                    ap_params.output_state_indices.values[(
+                                        input_state_number,
+                                        (1 + x_offset) as usize,
+                                        (1 + y_offset) as usize,
+                                        (1 + z_offset) as usize,
+                                        output_direction,
+                                    )] = Some(output_state_index);
+                                }
+                            }
                         }
                     }
                 }
