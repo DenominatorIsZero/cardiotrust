d1ea4cc429db4fe6d2551c2a36188a0c8a6b9067
Erik Engelhardt <eren@tf.uni-kiel.de>
2023-06-13 15:50:42 +0200

    Moved calculation of delay_samples array into delay module

--- src/core/model/functional/allpass.rs
+++ src/core/model/functional/allpass.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
 use approx::relative_eq;
 
+use itertools::Itertools;
 use ndarray::{arr1, s, Array1, Array3, Array4, ArrayBase, Dim, ViewRepr};
 
 use crate::core::{
@@ -9,7 +10,10 @@ use crate::core::{
     model::spatial::{voxels, voxels::VoxelType, SpatialDescription},
 };
 
-use self::shapes::{ArrayActivationTime, ArrayDelays, ArrayGains, ArrayIndicesGains};
+use self::{
+    delay::calculate_delay_samples_array,
+    shapes::{ArrayActivationTime, ArrayDelays, ArrayGains, ArrayIndicesGains},
+};
 
 mod delay;
 mod direction;
@@ -48,68 +52,10 @@ impl APParameters {
         );
         let mut current_directions =
             Array4::<f32>::zeros(spatial_description.voxels.positions_mm.values.raw_dim());
-        let mut delays_samples =
-            ArrayDelays::<f32>::empty(spatial_description.voxels.count_states());
 
         let v_types = &spatial_description.voxels.types.values;
         let v_position_mm = &spatial_description.voxels.positions_mm.values;
         let v_numbers = &spatial_description.voxels.numbers.values;
-
-        // Fill the delays_samples tensor
-        for (input_voxel_index, v_type) in v_types.indexed_iter() {
-            if *v_type == VoxelType::None {
-                continue;
-            }
-            let (x_in, y_in, z_in) = input_voxel_index;
-            let input_position_mm = &v_position_mm.slice(s![x_in, y_in, z_in, ..]);
-            for x_offset in -1..=1 {
-                for y_offset in -1..=1 {
-                    for z_offset in -1..=1 {
-                        if x_offset == 0 && y_offset == 0 && z_offset == 0 {
-                            continue;
-                        }
-                        let ouput_voxel_index = [
-                            x_in as i32 + x_offset,
-                            y_in as i32 + y_offset,
-                            z_in as i32 + z_offset,
-                        ];
-                        if !spatial_description.voxels.is_valid_index(ouput_voxel_index) {
-                            continue;
-                        }
-                        let [x_out, y_out, z_out] = [
-                            (x_in as i32 + x_offset) as usize,
-                            (y_in as i32 + y_offset) as usize,
-                            (z_in as i32 + z_offset) as usize,
-                        ];
-                        let output_position_mm = &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
-
-                        let delay_s = delay::calculate_delay_s(
-                            input_position_mm,
-                            output_position_mm,
-                            config.propagation_velocities_m_per_s.get(v_type).unwrap(),
-                        );
-                        let delay_samples = delay_s * sample_rate_hz;
-
-                        if delay_samples < 1.0 {
-                            return Err(format!(
-                                "Can not configure delays below 1 sample.\
-                        Calculated delay: {:?}.\
-                        For voxel type: {:?}",
-                                delay_samples, v_type
-                            )
-                            .into());
-                        }
-
-                        delays_samples.values[(
-                            v_numbers[input_voxel_index].unwrap() / 3,
-                            (1 + x_offset) as usize,
-                            (1 + y_offset) as usize,
-                            (1 + z_offset) as usize,
-                        )] = delay_samples;
-                    }
-                }
-            }
-        }
 
         let current_time_s: f32 = 0.0;
 
@@ -250,7 +196,6 @@ impl APParameters {
         }
 
         // init output state indices
-
         v_types
             .indexed_iter()
             .filter(|(_, v_type)| **v_type != VoxelType::None)
@@ -299,6 +244,12 @@ impl APParameters {
             .zip(activation_time_s)
             .filter(|(_, s)| s.is_some())
             .for_each(|(ms, s)| *ms = Some(s.unwrap() * 1000.0));
+
+        let delays_samples = calculate_delay_samples_array(
+            spatial_description,
+            &config.propagation_velocities_m_per_s,
+            sample_rate_hz,
+        )?;
 
         ap_params
             .delays
--- src/core/model/functional/allpass/delay.rs
+++ src/core/model/functional/allpass/delay.rs
@@ -1,4 +1,11 @@
-use ndarray::{ArrayBase, Dim, ViewRepr};
+use std::{collections::HashMap, error::Error};
+
+use itertools::Itertools;
+use ndarray::{s, ArrayBase, Dim, ViewRepr};
+
+use crate::core::model::spatial::{voxels::VoxelType, SpatialDescription};
+
+use super::shapes::ArrayDelays;
 
 pub fn calculate_delay_s(
     input_position_mm: &ArrayBase<ViewRepr<&f32>, Dim<[usize; 1]>>,
@@ -8,6 +15,73 @@ pub fn calculate_delay_s(
     let distance_m = (input_position_mm - output_position_mm) / 1000.0;
     let distance_norm_m = distance_m.mapv(|v| v.powi(2)).sum().sqrt();
     distance_norm_m / *propagation_velocity_m_per_s
+}
+
+pub fn calculate_delay_samples_array(
+    spatial_description: &SpatialDescription,
+    propagation_velocities_m_per_s: &HashMap<VoxelType, f32>,
+    sample_rate_hz: f32,
+) -> Result<ArrayDelays<f32>, Box<dyn Error>> {
+    let mut delays_samples = ArrayDelays::<f32>::empty(spatial_description.voxels.count_states());
+
+    let v_types = &spatial_description.voxels.types.values;
+    let v_position_mm = &spatial_description.voxels.positions_mm.values;
+    let v_numbers = &spatial_description.voxels.numbers.values;
+
+    // Fill the delays_samples tensor
+    for (input_voxel_index, v_type) in v_types.indexed_iter() {
+        if *v_type == VoxelType::None {
+            continue;
+        }
+        let (x_in, y_in, z_in) = input_voxel_index;
+        let input_position_mm = &v_position_mm.slice(s![x_in, y_in, z_in, ..]);
+        for ((x_offset, y_offset), z_offset) in
+            (-1..=1).cartesian_product(-1..=1).cartesian_product(-1..=1)
+        {
+            if x_offset == 0 && y_offset == 0 && z_offset == 0 {
+                continue;
+            }
+            let ouput_voxel_index = [
+                x_in as i32 + x_offset,
+                y_in as i32 + y_offset,
+                z_in as i32 + z_offset,
+            ];
+            if !spatial_description.voxels.is_valid_index(ouput_voxel_index) {
+                continue;
+            }
+            let [x_out, y_out, z_out] = [
+                (x_in as i32 + x_offset) as usize,
+                (y_in as i32 + y_offset) as usize,
+                (z_in as i32 + z_offset) as usize,
+            ];
+            let output_position_mm = &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
+
+            let delay_s = calculate_delay_s(
+                input_position_mm,
+                output_position_mm,
+                propagation_velocities_m_per_s.get(v_type).unwrap(),
+            );
+            let delay_samples = delay_s * sample_rate_hz;
+
+            if delay_samples < 1.0 {
+                return Err(format!(
+                    "Can not configure delays below 1 sample.\
+                        Calculated delay: {:?}.\
+                        For voxel type: {:?}",
+                    delay_samples, v_type
+                )
+                .into());
+            }
+
+            delays_samples.values[(
+                v_numbers[input_voxel_index].unwrap() / 3,
+                (1 + x_offset) as usize,
+                (1 + y_offset) as usize,
+                (1 + z_offset) as usize,
+            )] = delay_samples;
+        }
+    }
+    Ok(delays_samples)
 }
 
 #[cfg(test)]
