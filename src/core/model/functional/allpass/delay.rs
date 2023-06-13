50ff6efc6a8240daaf4bb2c0e9b3dd953abbee9e
Erik Engelhardt <eren@tf.uni-kiel.de>
2023-06-13 15:12:13 +0200

    Added tests for calculation of delay

--- src/core/model/functional/allpass/delay.rs
+++ src/core/model/functional/allpass/delay.rs
@@ -9,3 +9,41 @@ pub fn calculate_delay_s(
     let distance_norm_m = distance_m.mapv(|v| v.powi(2)).sum().sqrt();
     distance_norm_m / *propagation_velocity_m_per_s
 }
+
+#[cfg(test)]
+mod test {
+    use approx::assert_relative_eq;
+    use ndarray::{arr1, Array1};
+
+    use super::calculate_delay_s;
+
+    #[test]
+    fn calculate_delay_s_1() {
+        let input_position_mm: Array1<f32> = arr1(&[1000.0, 0.0, 0.0]);
+        let output_position_mm: Array1<f32> = arr1(&[2000.0, 0.0, 0.0]);
+        let propagation_velocity_m_per_s = 2.0;
+
+        let delay_s = calculate_delay_s(
+            &input_position_mm.view(),
+            &output_position_mm.view(),
+            &propagation_velocity_m_per_s,
+        );
+
+        assert_relative_eq!(delay_s, 0.5)
+    }
+
+    #[test]
+    fn calculate_delay_s_2() {
+        let input_position_mm: Array1<f32> = arr1(&[1000.0, 0.0, 0.0]);
+        let output_position_mm: Array1<f32> = arr1(&[4000.0, 4000.0, 0.0]);
+        let propagation_velocity_m_per_s = 2.0;
+
+        let delay_s = calculate_delay_s(
+            &input_position_mm.view(),
+            &output_position_mm.view(),
+            &propagation_velocity_m_per_s,
+        );
+
+        assert_relative_eq!(delay_s, 2.5)
+    }
+}
