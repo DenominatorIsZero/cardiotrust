0ba37697458353d4b63463e2f61bd2fbed1cf39a
eren <erik.raik.engelhardt@gmail.com>
2023-07-02 20:36:42 +0200

    commented out writing of unused file types

--- src/vis/plotting.rs
+++ src/vis/plotting.rs
@@ -5,7 +5,7 @@ pub mod matrix;
 pub mod time;
 
 fn save_plot(file_name: &str, plot: Plot, width: usize, height: usize, scale: f64) {
-    plot.write_html(format!("{file_name}.html"));
+    // plot.write_html(format!("{file_name}.html"));
     plot.write_image(
         format!("{file_name}.png"),
         plotly::ImageFormat::PNG,
@@ -13,18 +13,18 @@ fn save_plot(file_name: &str, plot: Plot, width: usize, height: usize, scale: f6
         height,
         scale,
     );
-    plot.write_image(
-        format!("{file_name}.svg"),
-        plotly::ImageFormat::SVG,
-        width,
-        height,
-        scale,
-    );
-    plot.write_image(
-        format!("{file_name}.pdf"),
-        plotly::ImageFormat::PDF,
-        width,
-        height,
-        scale,
-    );
+    // plot.write_image(
+    //     format!("{file_name}.svg"),
+    //     plotly::ImageFormat::SVG,
+    //     width,
+    //     height,
+    //     scale,
+    // );
+    // plot.write_image(
+    //     format!("{file_name}.pdf"),
+    //     plotly::ImageFormat::PDF,
+    //     width,
+    //     height,
+    //     scale,
+    // );
 }
