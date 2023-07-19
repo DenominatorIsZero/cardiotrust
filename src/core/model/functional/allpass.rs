aee8766ddf2f923839ad35288b4809bdcc928704
eren <erik.raik.engelhardt@gmail.com>
2023-07-19 17:40:36 +0200

    refactor, removed warnings

--- src/core/model/functional/allpass.rs
+++ src/core/model/functional/allpass.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, ops::ControlFlow};
 
 use approx::relative_eq;
 
@@ -42,177 +42,23 @@ impl APParameters {
         }
     }
 
+    /// .
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if model cant be build with
+    /// given config.
     pub fn from_model_config(
         config: &Model,
         spatial_description: &SpatialDescription,
         sample_rate_hz: f32,
-    ) -> Result<APParameters, Box<dyn Error>> {
-        let voxels_in_dims = spatial_description.voxels.types.values.raw_dim();
-        let mut ap_params =
-            APParameters::empty(spatial_description.voxels.count_states(), voxels_in_dims);
-        let mut activation_time_s = Array3::<Option<f32>>::from_elem(
+    ) -> Result<Self, Box<dyn Error>> {
+        let mut ap_params = Self::empty(
+            spatial_description.voxels.count_states(),
             spatial_description.voxels.types.values.raw_dim(),
-            None,
         );
-        let mut current_directions =
-            Array4::<f32>::zeros(spatial_description.voxels.positions_mm.values.raw_dim());
 
-        let v_types = &spatial_description.voxels.types.values;
-        let v_position_mm = &spatial_description.voxels.positions_mm.values;
-        let v_numbers = &spatial_description.voxels.numbers.values;
-
-        // TODO: Extract into function
-        // TODO: write tests
-        let mut current_time_s: f32 = 0.0;
-        // Handle Sinoatrial node
-        v_types
-            .indexed_iter()
-            .filter(|(_, v_type)| **v_type == VoxelType::Sinoatrial)
-            .for_each(|(index, _)| {
-                activation_time_s[index] = Some(current_time_s);
-                current_directions
-                    .slice_mut(s![index.0, index.1, index.2, ..])
-                    .assign(&arr1(&[1.0, 0.0, 0.0]));
-            });
-        let mut connected_something = true;
-
-        while connected_something {
-            // reset the connected something variable so we don't get stuck here forever
-            // have to check the activation times because there might be come connection possible
-            // with a voxel that is not yet activated.
-            if !activation_time_s
-                .iter()
-                .filter(|time_s| time_s.is_some())
-                .any(|time_s| time_s.unwrap() > current_time_s)
-            {
-                connected_something = false;
-            }
-            // find all voxels with an activation time equal to the current time
-            // i.e., currently activated voxels
-            let output_voxel_indices: Vec<(usize, usize, usize)> = activation_time_s
-                .indexed_iter()
-                .filter(|(_, time_s)| {
-                    time_s.is_some() && relative_eq!(time_s.unwrap(), current_time_s)
-                })
-                .map(|(index, _)| index)
-                .collect();
-
-            for output_voxel_index in output_voxel_indices {
-                for x_offset in -1..=1 {
-                    for y_offset in -1..=1 {
-                        for z_offset in -1..=1 {
-                            // no self connection allowed
-                            if x_offset == 0 && y_offset == 0 && z_offset == 0 {
-                                continue;
-                            }
-                            let (x_out, y_out, z_out) = output_voxel_index;
-                            let input_voxel_index = [
-                                x_out as i32 - x_offset,
-                                y_out as i32 - y_offset,
-                                z_out as i32 - z_offset,
-                            ];
-                            // Skip if the input voxel doesn't exist
-                            if !spatial_description.voxels.is_valid_index(input_voxel_index) {
-                                continue;
-                            }
-                            let input_voxel_index = [
-                                (x_out as i32 - x_offset) as usize,
-                                (y_out as i32 - y_offset) as usize,
-                                (z_out as i32 - z_offset) as usize,
-                            ];
-                            // SKip if the input voxel is already connected
-                            if activation_time_s[input_voxel_index].is_some() {
-                                continue;
-                            }
-                            // Skip if connection is not allowed
-                            let output_voxel_type = &v_types[output_voxel_index];
-                            let input_voxel_type = &v_types[input_voxel_index];
-                            if !voxels::is_connection_allowed(output_voxel_type, input_voxel_type) {
-                                continue;
-                            }
-                            // Skip pathologies as anways if the propagation factor is zero
-                            if input_voxel_type == &VoxelType::Pathological
-                                && relative_eq!(config.current_factor_in_pathology, 0.0)
-                            {
-                                continue;
-                            }
-
-                            // Now we finally found something that we want to connect.
-                            let input_state_number = v_numbers[input_voxel_index].unwrap();
-                            connected_something = true;
-
-                            let output_position_mm =
-                                &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
-                            let [x_in, y_in, z_in] = input_voxel_index;
-                            let input_position_mm = &v_position_mm.slice(s![x_in, y_in, z_in, ..]);
-                            let propagation_velocity_m_per_s = config
-                                .propagation_velocities_m_per_s
-                                .get(input_voxel_type)
-                                .unwrap();
-
-                            let delay_s = delay::calculate_delay_s(
-                                input_position_mm,
-                                output_position_mm,
-                                *propagation_velocity_m_per_s,
-                            );
-                            let direction =
-                                direction::calculate(input_position_mm, output_position_mm);
-
-                            // update activation time of input voxel, marking them as connected
-                            activation_time_s[input_voxel_index] =
-                                Some(activation_time_s[output_voxel_index].unwrap() + delay_s);
-
-                            current_directions
-                                .slice_mut(s![x_in, y_in, z_in, ..])
-                                .assign(&direction);
-
-                            let mut gain = gain::calculate(
-                                &direction,
-                                current_directions.slice(s![x_out, y_out, z_out, ..]),
-                            );
-
-                            if *input_voxel_type == VoxelType::Pathological
-                                && *output_voxel_type != VoxelType::Pathological
-                            {
-                                gain = gain * config.current_factor_in_pathology;
-                            }
-                            if *output_voxel_type == VoxelType::Pathological
-                                && *input_voxel_type != VoxelType::Pathological
-                            {
-                                gain = gain * (1.0 / config.current_factor_in_pathology);
-                            }
-
-                            for input_dimension in 0..3 {
-                                for output_dimension in 0..3 {
-                                    ap_params.gains.values[(
-                                        input_state_number + input_dimension,
-                                        (1 + x_offset) as usize,
-                                        (1 + y_offset) as usize,
-                                        (1 + z_offset) as usize,
-                                        output_dimension,
-                                    )] = gain[(input_dimension, output_dimension)];
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            let candidate_times_s: Vec<f32> = activation_time_s
-                .iter()
-                .filter(|t| t.is_some() && t.unwrap() > current_time_s)
-                .map(|t| t.unwrap())
-                .collect();
-            let candidate_times_s = Array1::from_vec(candidate_times_s);
-            current_time_s = *candidate_times_s.min_skipnan();
-        }
-
-        ap_params
-            .activation_time_ms
-            .values
-            .iter_mut()
-            .zip(activation_time_s)
-            .filter(|(_, s)| s.is_some())
-            .for_each(|(ms, s)| *ms = Some(s.unwrap() * 1000.0));
+        connect_voxels(spatial_description, config, &mut ap_params);
 
         let delays_samples = calculate_delay_samples_array(
             spatial_description,
@@ -238,6 +84,208 @@ impl APParameters {
 
         Ok(ap_params)
     }
+}
+
+fn connect_voxels(
+    spatial_description: &SpatialDescription,
+    config: &Model,
+    ap_params: &mut APParameters,
+) {
+    let mut activation_time_s =
+        Array3::<Option<f32>>::from_elem(spatial_description.voxels.types.values.raw_dim(), None);
+    let mut current_directions =
+        Array4::<f32>::zeros(spatial_description.voxels.positions_mm.values.raw_dim());
+
+    let v_types = &spatial_description.voxels.types.values;
+
+    // TODO: Extract into function
+    // TODO: write tests
+    let mut current_time_s: f32 = 0.0;
+    // Handle Sinoatrial node
+    v_types
+        .indexed_iter()
+        .filter(|(_, v_type)| **v_type == VoxelType::Sinoatrial)
+        .for_each(|(index, _)| {
+            activation_time_s[index] = Some(current_time_s);
+            current_directions
+                .slice_mut(s![index.0, index.1, index.2, ..])
+                .assign(&arr1(&[1.0, 0.0, 0.0]));
+        });
+    let mut connected_something = true;
+
+    while connected_something {
+        // reset the connected something variable so we don't get stuck here forever
+        // have to check the activation times because there might be come connection possible
+        // with a voxel that is not yet activated.
+        if !activation_time_s
+            .iter()
+            .filter(|time_s| time_s.is_some())
+            .any(|time_s| time_s.unwrap() > current_time_s)
+        {
+            connected_something = false;
+        }
+        // find all voxels with an activation time equal to the current time
+        // i.e., currently activated voxels
+        let output_voxel_indices = find_candidate_voxels(&activation_time_s, current_time_s);
+
+        for output_voxel_index in output_voxel_indices {
+            for x_offset in -1..=1 {
+                for y_offset in -1..=1 {
+                    for z_offset in -1..=1 {
+                        connected_something |= try_to_connect(
+                            (x_offset, y_offset, z_offset),
+                            output_voxel_index,
+                            spatial_description,
+                            &mut activation_time_s,
+                            config,
+                            &mut current_directions,
+                            ap_params,
+                        );
+                    }
+                }
+            }
+        }
+        let candidate_times_s: Vec<f32> = activation_time_s
+            .iter()
+            .filter(|t| t.is_some() && t.unwrap() > current_time_s)
+            .map(|t| t.unwrap())
+            .collect();
+        let candidate_times_s = Array1::from_vec(candidate_times_s);
+        current_time_s = *candidate_times_s.min_skipnan();
+    }
+    ap_params
+        .activation_time_ms
+        .values
+        .iter_mut()
+        .zip(activation_time_s)
+        .filter(|(_, s)| s.is_some())
+        .for_each(|(ms, s)| *ms = Some(s.unwrap() * 1000.0));
+}
+
+fn try_to_connect(
+    voxel_offset: (i32, i32, i32),
+    output_voxel_index: (usize, usize, usize),
+    spatial_description: &SpatialDescription,
+    activation_time_s: &mut ndarray::ArrayBase<ndarray::OwnedRepr<Option<f32>>, Dim<[usize; 3]>>,
+    config: &Model,
+    current_directions: &mut ndarray::ArrayBase<ndarray::OwnedRepr<f32>, Dim<[usize; 4]>>,
+    ap_params: &mut APParameters,
+) -> bool {
+    let v_types = &spatial_description.voxels.types.values;
+    let v_position_mm = &spatial_description.voxels.positions_mm.values;
+    let v_numbers = &spatial_description.voxels.numbers.values;
+    let (x_offset, y_offset, z_offset) = voxel_offset;
+
+    // no self connection allowed
+    if x_offset == 0 && y_offset == 0 && z_offset == 0 {
+        return false;
+    }
+    let (x_out, y_out, z_out) = output_voxel_index;
+    let input_voxel_index = [
+        i32::try_from(x_out).unwrap() - x_offset,
+        i32::try_from(y_out).unwrap() - y_offset,
+        i32::try_from(z_out).unwrap() - z_offset,
+    ];
+    // Skip if the input voxel doesn't exist
+    if !spatial_description.voxels.is_valid_index(input_voxel_index) {
+        return false;
+    }
+    let input_voxel_index = [
+        (x_out as i32 - x_offset) as usize,
+        (y_out as i32 - y_offset) as usize,
+        (z_out as i32 - z_offset) as usize,
+    ];
+    // SKip if the input voxel is already connected
+    if activation_time_s[input_voxel_index].is_some() {
+        return false;
+    }
+    let output_voxel_type = &v_types[output_voxel_index];
+    let input_voxel_type = &v_types[input_voxel_index];
+    // Skip if connection is not alowed
+    if !voxels::is_connection_allowed(output_voxel_type, input_voxel_type) {
+        return false;
+    }
+    // Skip pathologies if the propagation factor is zero
+    if input_voxel_type == &VoxelType::Pathological
+        && relative_eq!(config.current_factor_in_pathology, 0.0)
+    {
+        return false;
+    }
+    // Now we finally found something that we want to connect.
+    let input_state_number = v_numbers[input_voxel_index].unwrap();
+    let output_position_mm = &v_position_mm.slice(s![x_out, y_out, z_out, ..]);
+    let [x_in, y_in, z_in] = input_voxel_index;
+    let input_position_mm = &v_position_mm.slice(s![x_in, y_in, z_in, ..]);
+    let propagation_velocity_m_per_s = config
+        .propagation_velocities_m_per_s
+        .get(input_voxel_type)
+        .unwrap();
+    let delay_s = delay::calculate_delay_s(
+        input_position_mm,
+        output_position_mm,
+        *propagation_velocity_m_per_s,
+    );
+    // update activation time of input voxel, marking them as connected
+    activation_time_s[input_voxel_index] =
+        Some(activation_time_s[output_voxel_index].unwrap() + delay_s);
+    let direction = direction::calculate(input_position_mm, output_position_mm);
+    current_directions
+        .slice_mut(s![x_in, y_in, z_in, ..])
+        .assign(&direction);
+    let mut gain = gain::calculate(
+        &direction,
+        current_directions.slice(s![x_out, y_out, z_out, ..]),
+    );
+    if *input_voxel_type == VoxelType::Pathological && *output_voxel_type != VoxelType::Pathological
+    {
+        gain = gain * config.current_factor_in_pathology;
+    }
+    if *output_voxel_type == VoxelType::Pathological && *input_voxel_type != VoxelType::Pathological
+    {
+        gain = gain * (1.0 / config.current_factor_in_pathology);
+    }
+    assign_gain(
+        ap_params,
+        input_state_number,
+        x_offset,
+        y_offset,
+        z_offset,
+        &gain,
+    );
+    return true;
+}
+
+fn assign_gain(
+    ap_params: &mut APParameters,
+    input_state_number: usize,
+    x_offset: i32,
+    y_offset: i32,
+    z_offset: i32,
+    gain: &ndarray::ArrayBase<ndarray::OwnedRepr<f32>, Dim<[usize; 2]>>,
+) {
+    for input_dimension in 0..3 {
+        for output_dimension in 0..3 {
+            ap_params.gains.values[(
+                input_state_number + input_dimension,
+                (1 + x_offset) as usize,
+                (1 + y_offset) as usize,
+                (1 + z_offset) as usize,
+                output_dimension,
+            )] = gain[(input_dimension, output_dimension)];
+        }
+    }
+}
+
+fn find_candidate_voxels(
+    activation_time_s: &ndarray::ArrayBase<ndarray::OwnedRepr<Option<f32>>, Dim<[usize; 3]>>,
+    current_time_s: f32,
+) -> Vec<(usize, usize, usize)> {
+    let output_voxel_indices: Vec<(usize, usize, usize)> = activation_time_s
+        .indexed_iter()
+        .filter(|(_, time_s)| time_s.is_some() && relative_eq!(time_s.unwrap(), current_time_s))
+        .map(|(index, _)| index)
+        .collect();
+    output_voxel_indices
 }
 
 fn init_output_state_indicies(spatial_description: &SpatialDescription) -> ArrayIndicesGains {
