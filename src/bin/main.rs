3d3199ea0d8e7ea22e5f9a502d82324385bc59ff
eren <erik.raik.engelhardt@gmail.com>
2023-10-14 22:21:06 +0200

    First contact with aabb library

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,11 @@
 use bevy::{prelude::*, window::WindowMode};
+use bevy_aabb_instancing::{
+    Cuboid, CuboidMaterial, CuboidMaterialId, CuboidMaterialMap, Cuboids, CuboidsBundle,
+    VertexPullingRenderPlugin,
+};
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use rayon::iter::IntoParallelRefMutIterator;
+use rayon::iter::ParallelIterator;
 
 use rusty_cde::{scheduler::SchedulerPlugin, ui::UiPlugin, ScenarioList, SelectedSenario};
 
@@ -7,6 +13,7 @@ fn main() {
     App::new()
         .init_resource::<ScenarioList>()
         .init_resource::<SelectedSenario>()
+        .insert_resource(Msaa::Off)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Rusty CDE".into(),
@@ -18,7 +25,9 @@ fn main() {
         .add_plugins(PanOrbitCameraPlugin)
         .add_plugins(UiPlugin)
         .add_plugins(SchedulerPlugin)
+        .add_plugins(VertexPullingRenderPlugin { outlines: true })
         .add_systems(Startup, setup)
+        .add_systems(Update, update_cuboids_colors)
         .run();
 }
 
@@ -27,19 +36,6 @@ pub fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.5),
-        ..default()
-    });
-
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
@@ -57,4 +53,55 @@ pub fn setup(
         },
         PanOrbitCamera::default(),
     ));
+
+    const PATCHES_PER_DIM: usize = 20;
+    const PATCH_SIZE: usize = 15;
+    const SCENE_RADIUS: f32 = 150.0;
+
+    for x_batch in 0..PATCHES_PER_DIM {
+        for z_batch in 0..PATCHES_PER_DIM {
+            let mut instances = Vec::with_capacity(PATCH_SIZE * PATCH_SIZE);
+            for x in 0..PATCH_SIZE {
+                for z in 0..PATCH_SIZE {
+                    let x = (x_batch * PATCH_SIZE) as f32 + x as f32 - SCENE_RADIUS;
+                    let z = (z_batch * PATCH_SIZE) as f32 + z as f32 - SCENE_RADIUS;
+                    let d = (x * x + z * z).sqrt();
+                    let amp = 0.2 * d;
+                    let y = amp * ((0.05 * x).cos() * (0.05 * z).sin());
+                    let c = Vec3::new(x, y, z);
+                    let h = 0.01 * d;
+                    let min = c - Vec3::new(0.5, h, 0.5);
+                    let max = c + Vec3::new(0.5, h, 0.5);
+                    let scalar_color = Color::as_rgba_u32(Color::Rgba {
+                        red: min.x / SCENE_RADIUS,
+                        green: min.z / SCENE_RADIUS,
+                        blue: 0.0,
+                        alpha: 1.0,
+                    });
+                    let mut cuboid = Cuboid::new(min, max, scalar_color);
+                    cuboid.set_depth_bias(0);
+                    instances.push(cuboid);
+                }
+            }
+            let cuboids = Cuboids::new(instances);
+            let aabb = cuboids.aabb();
+            commands
+                .spawn(SpatialBundle::default())
+                .insert((cuboids, aabb, CuboidMaterialId(0)));
+        }
+    }
+}
+
+fn update_cuboids_colors(time: Res<Time>, mut query: Query<&mut Cuboids>) {
+    let tv: u32 = (1000.0 * (time.elapsed_seconds().sin() + 1.0)) as u32;
+    for mut cuboids in query.iter_mut() {
+        cuboids.instances.par_iter_mut().for_each(|instance| {
+            instance.color = Color::as_rgba_u32(Color::Rgba {
+                red: ((time.elapsed_seconds() * 130.0 + instance.minimum.x) / 7.0).sin() + 1.0,
+                green: ((time.elapsed_seconds() * 70.0 + instance.minimum.z) / 13.0).sin() + 1.0,
+                blue: ((time.elapsed_seconds() * 30.0 + instance.minimum.y) / 5.0).sin() + 1.0,
+                alpha: 1.0,
+            })
+        })
+    }
 }
