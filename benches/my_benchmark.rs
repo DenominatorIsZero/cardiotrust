d7d916a746d348dff2a36da85c3e77f3f7d31071
eren <erik.raik.engelhardt@gmail.com>
2023-11-17 14:14:14 +0100

    Added Epoch Benches

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -7,7 +7,7 @@ static GLOBAL: Jemalloc = Jemalloc;
 
 use std::time::Duration;
 
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use rusty_cde::core::algorithm::estimation::{
     add_control_function, calculate_system_prediction, innovate_system_states, predict_measurements,
 };
@@ -91,7 +91,91 @@ fn system_prediction(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group! {name = benches;
+fn system_prediction_epoch(c: &mut Criterion) {
+    let mut group = c.benchmark_group("System Prediction Epoch");
+    for voxel_size in VOXEL_SIZES.iter() {
+        let samplerate_hz = 2000.0 * 2.5 / voxel_size;
+        let mut config = Config::default();
+        config.simulation.as_mut().unwrap().model.voxel_size_mm = *voxel_size;
+        config.simulation.as_mut().unwrap().sample_rate_hz = samplerate_hz;
+        config.algorithm.model.voxel_size_mm = *voxel_size;
+        let simulation_config = config.simulation.as_ref().unwrap();
+        let data = Data::from_simulation_config(simulation_config);
+        let model = Model::from_model_config(
+            &config.algorithm.model,
+            simulation_config.sample_rate_hz,
+            simulation_config.duration_s,
+        )
+        .unwrap();
+        let mut estimations = Estimations::empty(
+            model.spatial_description.voxels.count_states(),
+            model.spatial_description.sensors.count(),
+            data.get_measurements().values.shape()[0],
+        );
+        let number_of_voxels = model.spatial_description.voxels.count();
+        group.throughput(criterion::Throughput::Elements(number_of_voxels as u64));
+        group.bench_function(
+            BenchmarkId::new("calculate_system_perdiction", voxel_size),
+            |b| {
+                b.iter(|| {
+                    for time_index in 0..estimations.measurements.values.shape()[0] {
+                        calculate_system_prediction(
+                            &mut estimations.ap_outputs,
+                            &mut estimations.system_states,
+                            &mut estimations.measurements,
+                            &model.functional_description,
+                            time_index,
+                        )
+                    }
+                })
+            },
+        );
+        group.bench_function(
+            BenchmarkId::new("innovate_system_states", voxel_size),
+            |b| {
+                b.iter(|| {
+                    for time_index in 0..estimations.measurements.values.shape()[0] {
+                        innovate_system_states(
+                            &mut estimations.ap_outputs,
+                            &model.functional_description,
+                            time_index,
+                            &mut estimations.system_states,
+                        )
+                    }
+                })
+            },
+        );
+        group.bench_function(BenchmarkId::new("add_control_function", voxel_size), |b| {
+            b.iter(|| {
+                for time_index in 0..estimations.measurements.values.shape()[0] {
+                    add_control_function(
+                        &model.functional_description,
+                        time_index,
+                        &mut estimations.system_states,
+                    )
+                }
+            })
+        });
+        group.bench_function(BenchmarkId::new("predict_measurements", voxel_size), |b| {
+            b.iter(|| {
+                for time_index in 0..estimations.measurements.values.shape()[0] {
+                    predict_measurements(
+                        &mut estimations.measurements,
+                        time_index,
+                        &model.functional_description.measurement_matrix,
+                        &mut estimations.system_states,
+                    )
+                }
+            })
+        });
+    }
+    group.finish();
+}
+
+criterion_group! {name = sample_benches;
 config = Criterion::default().measurement_time(Duration::from_secs(10));
 targets=system_prediction}
-criterion_main!(benches);
+criterion_group! {name = epoch_benches;
+config = Criterion::default().measurement_time(Duration::from_secs(10)).sample_size(10);
+targets=system_prediction_epoch}
+criterion_main!(sample_benches, epoch_benches);
