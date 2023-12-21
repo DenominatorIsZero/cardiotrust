d4aa56b7ad18ef0ad3c2248b7307f89bbc3ea3dd
eren <erik.raik.engelhardt@gmail.com>
2023-12-21 16:27:51 +0100

    Enabled fullscreen for wasm client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,16 +1,27 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
 use rusty_cde::{
     ui::ClientUiPlugin, vis::VisPlugin, websocket::WebsocketPlugin, ScenarioList, SelectedSenario,
 };
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(bevy::log::LogPlugin {
-            // Uncomment this to override the default log settings:
-            // level: bevy::log::Level::TRACE,
-            // filter: "wgpu=warn,bevy_ecs=info".to_string(),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(bevy::log::LogPlugin {
+                    // Uncomment this to override the default log settings:
+                    // level: bevy::log::Level::TRACE,
+                    // filter: "wgpu=warn,bevy_ecs=info".to_string(),
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        present_mode: PresentMode::AutoNoVsync, // Reduces input lag.
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .init_resource::<SelectedSenario>()
         .insert_resource(ScenarioList::empty())
         .add_plugins(WebsocketPlugin)
