cbd33e0eef3efb922a51909e94aef5a4559cf8b8
Erik Engelhardt <eren@tf.uni-kiel.de>
2024-05-28 17:37:06 +0200

    Update: planner

--- src/bin/planner.rs
+++ src/bin/planner.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::petgraph::algo};
 
 use tracing::info;
 use tracing_subscriber::fmt;
@@ -50,9 +50,9 @@ fn main() {
 #[tracing::instrument(level = "info")]
 fn plan_scenarios() {
     let learning_rate = 100.0;
-    let steps = 6_000;
+    let steps = 30_000;
     let batch_size = 1;
-    let experiment_name = "Moving_Sensors_2023_05_13";
+    let experiment_name = "Moving_Sensors_2023_05_28";
     let regularization_strength = 1.0;
     let regularization_threshold = 1.001;
     let optimizer = Optimizer::Sgd;
@@ -75,7 +75,7 @@ fn plan_scenarios() {
 
     let mut simulation_config = Simulation::default();
     simulation_config.model.common.pathological = true;
-    simulation_config.model.common.sensor_array_origin_mm = [100.0, -100.0, 325.0];
+    simulation_config.model.common.sensor_array_origin_mm = [0.0, -225.0, 25.0];
     simulation_config.model.common.sensor_array_motion = SensorArrayMotion::Static;
     simulation_config.model.common.measurement_covariance_mean = 1e-20;
 
@@ -85,15 +85,33 @@ fn plan_scenarios() {
     scenario.schedule().expect("Scheduling to succeed");
     scenario.save().expect("Scenario to save");
 
-    for y_step_exp in 1..=10 {
-        let y_step = 2_usize.pow(y_step_exp) + 1;
-        simulation_config.model.common.sensor_array_motion_steps = [1, y_step, 1];
-        simulation_config.model.common.sensor_array_origin_mm = [100.0, -400.0, 325.0];
-        simulation_config.model.common.sensor_array_motion_range_mm = [0.0, 600.0, 0.0];
-        algorithm_config.epochs = (steps as f32 / y_step as f32).ceil() as usize;
+    if false {
+        for y_step_exp in 1..=10 {
+            let y_step = 2_usize.pow(y_step_exp) + 1;
+            simulation_config.model.common.sensor_array_motion_steps = [1, y_step, 1];
+            simulation_config.model.common.sensor_array_origin_mm = [0.0, -525.0, 25.0];
+            simulation_config.model.common.sensor_array_motion_range_mm = [0.0, 600.0, 0.0];
+            algorithm_config.epochs = (steps as f32 / y_step as f32).ceil() as usize;
+            simulation_config.model.common.sensor_array_motion = SensorArrayMotion::Grid;
+            let mut scenario = Scenario::build(Some(format!(
+                "{experiment_name} - (II) - Move Along Y - {y_step:0>4} Steps"
+            )));
+            scenario.config.algorithm = algorithm_config.clone();
+            scenario.config.simulation = simulation_config.clone();
+            scenario.schedule().expect("Scheduling to succeed");
+            scenario.save().expect("Scenario to save");
+        }
+    }
+
+    for step in 2..=10_usize {
+        let total_steps = step.pow(3);
+        simulation_config.model.common.sensor_array_motion_steps = [step, step, step];
+        simulation_config.model.common.sensor_array_origin_mm = [-75.0, -525.0, -25.0];
+        simulation_config.model.common.sensor_array_motion_range_mm = [150.0, 600.0, 150.0];
+        algorithm_config.epochs = (steps as f32 / total_steps as f32).round() as usize;
         simulation_config.model.common.sensor_array_motion = SensorArrayMotion::Grid;
         let mut scenario = Scenario::build(Some(format!(
-            "{experiment_name} - (II) - Move Along Y - {y_step:0>4} Steps"
+            "{experiment_name} - (II) - Move Along XYZ - {total_steps:0>4} Steps"
         )));
         scenario.config.algorithm = algorithm_config.clone();
         scenario.config.simulation = simulation_config.clone();
@@ -101,18 +119,17 @@ fn plan_scenarios() {
         scenario.save().expect("Scenario to save");
     }
 
-    for step in 2..=10_usize {
-        let total_steps = step.pow(3);
-        if step > 4 {
-            algorithm_config.learning_rate = 1.0;
-        }
+    for lr_exp in -3..=4 {
+        let total_steps = 10.pow(3);
         simulation_config.model.common.sensor_array_motion_steps = [step, step, step];
-        simulation_config.model.common.sensor_array_origin_mm = [-100.0, -400.0, -25.0];
-        simulation_config.model.common.sensor_array_motion_range_mm = [300.0, 600.0, 600.0];
+        simulation_config.model.common.sensor_array_origin_mm = [-75.0, -525.0, -25.0];
+        simulation_config.model.common.sensor_array_motion_range_mm = [150.0, 600.0, 150.0];
         algorithm_config.epochs = (steps as f32 / total_steps as f32).round() as usize;
+        let lr = 10.0.exp(lr_exp);
+        algorithm_config.learning_rate = 1.0;
         simulation_config.model.common.sensor_array_motion = SensorArrayMotion::Grid;
         let mut scenario = Scenario::build(Some(format!(
-            "{experiment_name} - (III) - Move Along XYZ - {total_steps:0>4} Steps"
+            "{experiment_name} - (III) - Move Along XYZ (LR Sweep)- {lr} LR"
         )));
         scenario.config.algorithm = algorithm_config.clone();
         scenario.config.simulation = simulation_config.clone();
