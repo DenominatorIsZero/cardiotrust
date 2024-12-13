f6c917db90a78a4d63b780c6a48d75c4ca869933
eren <erik.raik.engelhardt@gmail.com>
2024-12-13 15:25:45 +0100

    Implemented display trait for Optimizer

--- src/core/algorithm/refinement.rs
+++ src/core/algorithm/refinement.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 pub mod derivation;
 pub mod update;
@@ -7,4 +9,13 @@ pub enum Optimizer {
     #[default]
     Sgd,
     Adam,
+}
+
+impl Display for Optimizer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Sgd => write!(f, "SGD"),
+            Self::Adam => write!(f, "Adam"),
+        }
+    }
 }
