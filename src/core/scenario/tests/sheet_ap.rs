41a532254c318d319abeacb7e79e635a216f6d07
eren <erik.raik.engelhardt@gmail.com>
2025-01-06 14:45:08 +0100

    First shot at sheep ap

--- src/core/scenario/tests/sheet_ap.rs
+++ src/core/scenario/tests/sheet_ap.rs
@@ -23,32 +23,14 @@ const COMMON_PATH: &str = "tests/core/scenario/sheet_ap/";
 )]
 #[test]
 #[ignore]
-fn heavy_no_roll_down() {
-    let base_id = "Sheet AP - No Roll - Down - ".to_string();
-    let base_title = "Sheet AP - No Roll - Down";
-    let path = Path::new(COMMON_PATH).join("no_roll_down");
+fn heavy_homogeneous_up() {
+    let base_id = "Sheet AP - Homogenous - Up - ";
+    let path = Path::new(COMMON_PATH).join("homogeneous_up");
 
-    let integer_part = 5.0;
-    let voxel_size_mm = 2.5;
-    let sample_rate_hz = 2000.0;
+    let initial_delay = 4.1;
+    let target_delay = 4.2;
 
-    let initial_fractional = 0.5;
-    let initial_delay = integer_part + initial_fractional;
-    let initial_delay_s = initial_delay / sample_rate_hz;
-    let initial_velocity = voxel_size_mm / 1000.0 / initial_delay_s;
-
-    let target_fractional = 0.55;
-    let target_delay = integer_part + target_fractional;
-    let target_delay_s = target_delay / sample_rate_hz;
-    let target_velocity = voxel_size_mm / 1000.0 / target_delay_s;
-
-    create_and_run(
-        target_velocity,
-        initial_velocity,
-        &base_id,
-        &path,
-        base_title,
-    );
+    create_and_run(initial_delay, target_delay, base_id, &path);
 }
 
 #[allow(
@@ -59,32 +41,14 @@ fn heavy_no_roll_down() {
 )]
 #[test]
 #[ignore]
-fn heavy_no_roll_up() {
-    let base_id = "Sheet AP - No Roll - Up - ".to_string();
-    let base_title = "Sheet AP - No Roll - Up";
-    let path = Path::new(COMMON_PATH).join("no_roll_up");
+fn heavy_homogeneous_down() {
+    let base_id = "Sheet AP - Homogenous - Down - ";
+    let path = Path::new(COMMON_PATH).join("homogeneous_down");
 
-    let integer_part = 5.0;
-    let voxel_size_mm = 2.5;
-    let sample_rate_hz = 2000.0;
+    let initial_delay = 4.2;
+    let target_delay = 4.1;
 
-    let initial_fractional = 0.55;
-    let initial_delay = integer_part + initial_fractional;
-    let initial_delay_s = initial_delay / sample_rate_hz;
-    let initial_velocity = voxel_size_mm / 1000.0 / initial_delay_s;
-
-    let target_fractional = 0.5;
-    let target_delay = integer_part + target_fractional;
-    let target_delay_s = target_delay / sample_rate_hz;
-    let target_velocity = voxel_size_mm / 1000.0 / target_delay_s;
-
-    create_and_run(
-        target_velocity,
-        initial_velocity,
-        &base_id,
-        &path,
-        base_title,
-    );
+    create_and_run(initial_delay, target_delay, base_id, &path);
 }
 
 #[allow(
@@ -94,13 +58,12 @@ fn heavy_no_roll_up() {
     clippy::too_many_lines
 )]
 #[tracing::instrument(level = "trace")]
-fn build_scenario(target_velocity: f32, initial_velocity: f32, base_id: &str) -> Scenario {
-    let mut scenario = Scenario::build(Some(format!(
-        "{base_id} {initial_velocity:.2} [m per s] to {target_velocity:.2} [m per s]"
-    )));
+fn build_scenario(initial_delay: f32, target_delay: f32, id: String) -> Scenario {
+    let mut scenario = Scenario::build(Some(id));
 
     // Set pathological true
     scenario.config.simulation.model.common.pathological = true;
+    //    scenario.config.simulation.duration_s = 0.2;
     scenario
         .config
         .simulation
@@ -149,6 +112,14 @@ fn build_scenario(target_velocity: f32, initial_velocity: f32, base_id: &str) ->
     // Copy settings to algorithm model
     scenario.config.algorithm.model = scenario.config.simulation.model.clone();
     // Adjust propagation velocities
+
+    let sample_rate_hz = scenario.config.simulation.sample_rate_hz;
+    let voxel_size_mm = scenario.config.simulation.model.common.voxel_size_mm;
+    let targe_delay_s = target_delay / sample_rate_hz;
+    let target_velocity = voxel_size_mm / 1000.0 / targe_delay_s;
+    let initial_delay_s = initial_delay / sample_rate_hz;
+    let initial_velocity = voxel_size_mm / 1000.0 / initial_delay_s;
+
     *scenario
         .config
         .simulation
@@ -183,11 +154,11 @@ fn build_scenario(target_velocity: f32, initial_velocity: f32, base_id: &str) ->
         .unwrap() = initial_velocity;
     // set optimization parameters
     scenario.config.algorithm.epochs = 5_000;
-    scenario.config.algorithm.learning_rate = 1e4;
+    scenario.config.algorithm.learning_rate = 1e3;
     scenario.config.algorithm.optimizer = Optimizer::Sgd;
     scenario.config.algorithm.freeze_delays = false;
     scenario.config.algorithm.freeze_gains = true;
-    let number_of_snapshots = 1000;
+    let number_of_snapshots = 100;
     scenario.config.algorithm.snapshots_interval =
         scenario.config.algorithm.epochs / number_of_snapshots;
 
@@ -252,17 +223,11 @@ fn plot_results(path: &Path, base_title: &str, scenario: &Scenario) {
 }
 
 #[tracing::instrument(level = "trace")]
-fn create_and_run(
-    target_velocity: f32,
-    initial_velocity: f32,
-    base_id: &str,
-    img_path: &Path,
-    base_title: &str,
-) {
+fn create_and_run(initial_delay: f32, target_delay: f32, base_id: &str, img_path: &Path) {
     let mut join_handles = Vec::new();
 
-    let id = format!("{base_id} {initial_velocity:.2} [m per s] to {target_velocity:.2} [m per s]");
-    let path = Path::new("results").join(id);
+    let id = format!("{base_id} {initial_delay:.2} [samples] to {target_delay:.2} [samples]");
+    let path = Path::new("results").join(&id);
     let mut scenario = if path.is_dir() {
         println!("Found scenario. Loading it!");
         let mut scenario = Scenario::load(path.as_path());
@@ -271,7 +236,7 @@ fn create_and_run(
         scenario
     } else {
         println!("Didn't find scenario. Building it!");
-        let scenario = build_scenario(target_velocity, initial_velocity, base_id);
+        let scenario = build_scenario(initial_delay, target_delay, id);
         if RUN_IN_TESTS {
             let send_scenario = scenario.clone();
             let (epoch_tx, _) = channel();
@@ -293,5 +258,5 @@ fn create_and_run(
         scenario.load_results();
     }
 
-    plot_results(img_path, base_title, &scenario);
+    plot_results(img_path, base_id, &scenario);
 }
